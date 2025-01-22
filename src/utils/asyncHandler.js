58e14aad58e2a888a239777dc964eeac313fd77a
udit kumar <uditvarma312003@gmail.com>
2025-01-22 17:00:21 +0530

    "added return keyword in asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler =(func)=>async (req,res,next)=>{
   try{
-    await func(req,res,next);
+   return  await func(req,res,next);
   }catch(error){
     res.status(500).json({
         success:false,
